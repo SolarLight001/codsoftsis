f37b58790363e5502cb34fec9922a5f85ffa50f9
SolarLight001 <zarza.angel.2024@cbtis-246.edu.mx>
2025-02-28 19:59:07 -0600

    Add files via upload

---
+++ Errores ISdSI.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[115]:
+
+
+try:
+    if 'Rodrigo' == rodrigo.nombre:
+        print("Tienes toda la razón")
+except NameError:
+    print("Eres Gay?")
+
+
+# In[117]:
+
+
+try:
+    if 'Estefanía' == estefanía.nombre:
+        print("Felicidades!!")
+except NameError:
+    print("Suerte la proxima!")
+
+
+# In[119]:
+
+
+try:
+    if 'Gabriel' == gabriel.nombre:
+        print("Tienes toda la razón...")
+except NameError:
+    print("No es cierto: Un texto argumentativo es todo texto oral o escrito en el que el autor se propone transmitir y defender una postura, un punto de vista o una opinión en torno a un tema específico. Por lo tanto, su objetivo fundamental es convencer al lector de que su postura es la correcta o la más conveniente.")
+
+
+# In[121]:
+
+
+try:
+    if 'Juan' == juan.nombre:
+        print("Felicidades!! no eres p*****o")
+except NameError:
+    print("Te pareces al @Juan Sanchez!")
+
+
+# In[156]:
+
+
+try:
+    if 'Cesar' == cesar.nombre:
+        print("Si como no")
+except NameError:
+    print("Hey chicos sabían que en terminos de...")
+
+
+# In[158]:
+
+
+try:
+    if 'CBTis' == cbtis.nombre:
+        print("Si, que ocupa?")
+except NameError:
+    print("No esta Bastardo deja de mamar...")
+
+
+# In[160]:
+
+
+try:
+    if 'Pirata' == pirata.nombre:
+        print("Jack NEgrow!?")
+except NameError:
+    print("Negron James!?...")
+
+
+# In[166]:
+
+
+try:
+    if 'Aldahir' == aldahir.nombre:
+        print("No")
+except NameError:
+    print("Callate alv Cesar")
+
+
+# In[168]:
+
+
+try:
+    if 'Sergio' == sergio.nombre:
+        print("Un Brawl?")
+except NameError:
+    print("No esta Bastardo deja de mamar...")
+
+
+# In[170]:
+
+
+try:
+    if 'Otoniel' == otoniel.nombre:
+        print("Si, pa")
+except NameError:
+    print("No mames...")
+
